b7cb9449e56d14ca2118b8cb5e3634ee32f16f77
James Geib <jpgeib2@gmail.com>
2020-03-12 17:10:30 -0700

    troubleshooting the axios call for stars

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const html = require("./generateHTML");
 const options = { format: 'Letter' };
 const inquirer = require("inquirer");
 const axios = require("axios");
+let data = {};
 
 // fs.readFileSync('./generateHTML.js', 'utf8');
 
@@ -32,20 +33,33 @@ function init() {
         const queryUrl = `https://api.github.com/users/${github}`;
         axios.get(queryUrl).then(function (res) {
             console.log('Github response: ' + res.data, color);
-        const profileHTML = html({color, ...res.data})
-            
-            pdf.create(profileHTML, options).toFile('./-Profile-Generator.pdf', function (err, res) {
+            const profileHTML = html({ color, ...res.data })
+
+            pdf.create(profileHTML, options).toFile('./Profile-Generator.pdf', function (err, res) {
                 if (err) return console.log(err);
                 console.log(res);
             });
-        
+            axios // axios call to get the stars
+                .get(`https://api.github.com/users/${github}/repos?per_page=100`)
+                .then((res) => {
+
+                    data.stars = 0;
+                    for (let i = 0; i < res.data.length; i++) {
+                        data.stars += res.data[i].stargazers_count;
+                    }
+                });
         }).catch(err => {
             console.log(`User not found - ${github}`);
             process.exit(1);
         });
+
+
+
+
     });
 
 
 };
 
 init();
+
